0d864bafc1cc37bb8e30af8e909cabd681dde102
Debargha Mukherjee <56204372+debargham14@users.noreply.github.com>
2021-03-24 11:54:14 +0530

    Add files via upload

---
+++ asgn1_4/solve.java
@@ -0,0 +1,32 @@
+package java_assignment.asgn1_4;
+
+import java.lang.Math;
+import java.util.ArrayList;
+
+//designing the Quote Generator class
+class QuoteGenerator {
+    private ArrayList <String> arr = new ArrayList<String> ();
+    QuoteGenerator() {
+        arr.add("God helps them that help themselves. -- Benjamin Franklin");
+        arr.add("Happiness is not a reward - it is consequence. Suffering is not a punishment - it is a result. -- Robert Green Ingersoll");
+        arr.add("Future. That period of time in which our affairs prosper, our friends are true and our happiness is assured. -- Ambrose Bierce");
+        arr.add("Honesty is the first chapter of the book of wisdom. --Thomas Jefferson");
+    }
+    public String giveQuote() {
+        double rand = Math.random();
+        int idx = (int)(rand * 10) % arr.size(); 
+        String quote = arr.get(idx);
+        return quote;
+    }
+}
+
+//class Test
+class QuoteOfTheDay {
+    public static void main (String args[]) {
+        QuoteGenerator qg = new QuoteGenerator();
+        String s = qg.giveQuote();
+        System.out.println(s);
+    }
+}
+
+
